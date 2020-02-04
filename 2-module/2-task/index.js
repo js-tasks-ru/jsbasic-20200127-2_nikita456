7f05118ebebc08bed6cac3a440aa5cc424ab6966
nikita <e6wuk1990@mail.ru>
2020-02-04 15:31:43 +0200

    решение  занятие 2 - три задачи

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -3,5 +3,15 @@
  * @param {Object} obj
  * @returns {Boolean}
  */
+
+
 function isEmpty(obj) {
+  if (Object.keys(obj).length == 0) {
+    return true;
+  }
+  return false;
 }
+
+let schedule = {};
+
+
--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,21 @@
 let calculator = {
-  // ваш код
+  atr1: '',
+  atr2: '',
+  read: function(a, b) {
+    this.atr1 = a;
+    this.atr2 = b;
+  },
+  sum() {
+    return this.atr1 + this.atr2;
+  },
+  mul() {
+    return this.atr1 * this.atr2;
+  },
 };
+
+calculator.read(3, 5);
+alert(calculator.sum()); // 8
+alert(calculator.mul()); // 15
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
