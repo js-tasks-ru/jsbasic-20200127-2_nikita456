00e1e8ece53a29ffce96899388a09c996fbda353
nikita <e6wuk1990@mail.ru>
2020-02-10 10:23:13 +0200

    решение 3-х задач в третьем модуле

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -2,5 +2,19 @@
  * @param   {{ name: string, age: number }[]} users
  * @returns {string[]}  объект
  */
+let vasya = { name: 'Вася', age: 25 };
+let petya = { name: 'Петя', age: 30 };
+let masha = { name: 'Маша', age: 28 };
+
+let users = [vasya, petya, masha];
+
 function namify(users) {
+  let x = [];
+  users.map((item) => {
+    x.push(item.name);
+  });
+  return x;
 }
+
+let names = namify(users); // ['Вася', 'Петя', 'Маша']
+
